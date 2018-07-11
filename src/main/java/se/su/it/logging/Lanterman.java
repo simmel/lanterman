0a38ac7605dcdc1bb261af0c2c1e6e11995443d8
Simon Lundström <simlu+github@su.se>
2018-07-11 08:57:00 +0200

    Return a response at /

--- src/main/java/se/su/it/logging/Lanterman.java
+++ src/main/java/se/su/it/logging/Lanterman.java
@@ -1,6 +1,16 @@
 package se.su.it.logging;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@SpringBootApplication
 class Lanterman {
+
+  @RequestMapping("/")
+  String home() {
+    return "My log hears things I cannot hear. But my log tells me about the sounds, about the new words. Even though it has stopped growing larger, my log is aware.\n";
+  }
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Lanterman.class, args);
