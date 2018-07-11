ac2a64077b2e980e8dcb2f80a1edc38a4450784a
Simon Lundström <simlu+github@su.se>
2018-07-11 09:01:34 +0200

    Add all the loggers

--- src/main/java/se/su/it/logging/Lanterman.java
+++ src/main/java/se/su/it/logging/Lanterman.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 class Lanterman extends SpringBootServletInitializer {
 
+  private static final java.util.logging.Logger JUL = java.util.logging.Logger.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+  private static final org.apache.commons.logging.Log JCL = org.apache.commons.logging.LogFactory.getLog(Thread.currentThread().getStackTrace()[1].getClassName());
+  private static final org.apache.logging.log4j.Logger log4j2 = org.apache.logging.log4j.LogManager.getLogger();
+  private static final org.slf4j.Logger slf4j = org.slf4j.LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(Lanterman.class);
@@ -16,6 +21,12 @@ class Lanterman extends SpringBootServletInitializer {
 
   @RequestMapping("/")
   String home() {
+    JCL.info("Hello from JCL");
+    JUL.info("Hello from JULI");
+    log4j2.info("Hello from Log4j2");
+    slf4j.info("Hello from SLF4j");
+    System.out.println("Hello from STDOUT");
+    System.err.println("Hello from STDERR");
     return "My log hears things I cannot hear. But my log tells me about the sounds, about the new words. Even though it has stopped growing larger, my log is aware.\n";
   }
 
